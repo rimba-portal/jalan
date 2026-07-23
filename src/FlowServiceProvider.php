3894627ed3783676cb790357089c417985122366
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-23 15:45:55 +0800

    Automated Update 2026-07-23 15:45

--- src/FlowServiceProvider.php
+++ src/FlowServiceProvider.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Rimba\Flow;
 
-use Rimba\Base\BitesServiceProvider;
+use Rimba\Base\Services\BitesServiceProvider;
 
 class FlowServiceProvider extends BitesServiceProvider
 {
