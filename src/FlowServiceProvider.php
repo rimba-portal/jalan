31651fb80f13b3cc0ad02187a89a66bd938f3887
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-28 16:06:11 +0800

    Release v1.2.2

--- src/FlowServiceProvider.php
+++ src/FlowServiceProvider.php
@@ -9,6 +9,7 @@ use Rimba\Base\Services\BitesServiceProvider;
 
 class FlowServiceProvider extends BitesServiceProvider
 {
+    protected string $configFile = __DIR__ . '/../config/bites.php';
 
     protected function bootPackage(): void
     {
