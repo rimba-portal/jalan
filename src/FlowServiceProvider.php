2cc45cd709ba4eb76167d85e4db3e32ad01633be
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-30 14:56:43 +0800

    Release v1.2.5

--- src/FlowServiceProvider.php
+++ src/FlowServiceProvider.php
@@ -6,16 +6,18 @@ namespace Rimba\Flow;
 
 use Rimba\Base\Services\BitesServiceProvider;
 
+
 class FlowServiceProvider extends BitesServiceProvider
 {
+
     protected function bootPackage(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         //
     }
-
     protected function registerPackage(): void
     {
         //
     }
+
 }
